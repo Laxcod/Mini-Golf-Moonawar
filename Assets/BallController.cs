7085b84993209c3663d5b196f92528c38adc5b40
Laxcod <90342920+Laxcod@users.noreply.github.com>
2023-05-14 21:34:30 +0700

    FIxed Ball Material And Aimline
    memperbaiki material bola golf dan arah dari aimline

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,90 +1,119 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Cinemachine;
+using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class BallController : MonoBehaviour , IPointerDownHandler
+public class BallController : MonoBehaviour, IPointerDownHandler
 {
+    // [SerializeField] LayerMask layerMask;
     [SerializeField] Rigidbody rb;
-    [SerializeField] float force; 
-     [SerializeField] LineRenderer aimLine;
+    [SerializeField] float force;
+    [SerializeField] LineRenderer aimLine;
     [SerializeField] Transform aimWorld;
     bool shoot;
     bool shootingMode;
-
     float forceFactor;
     Vector3 forceDirection;
     Ray ray;
     Plane plane;
 
+    int shootCount;
+
     public bool ShootingMode { get => shootingMode; }
+    public int ShootCount { get => shootCount; }
+    public UnityEvent<int> onBallShooted = new UnityEvent<int>();
 
     private void Update()
-     {
+    {
+        // if(Input.GetMouseButtonDown(0))
+        // {
+        //     var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //     if(Physics.Raycast(ray, out var hitInfo, 100, layerMask) && hitInfo.collider == col)
+        //     {
+        //         shoot = true;
+        //     }
+        // }
+
         if(shootingMode)
         {
-          if(Input.GetMouseButtonDown(0))  
-          {
-            aimLine.gameObject.SetActive(true);
-            aimWorld.gameObject.SetActive(true);
-            plane = new Plane(Vector3.up,this.transform.position);
-          }
-          else if(Input.GetMouseButton(0))
-          {
-                var mouseviewportPos = Camera.main.ScreenToViewportPoint (Input.mousePosition);
-                var ballViewPortPos = Camera.main.WorldToViewportPoint(this.transform.position);
-                var ballScreenPos = Camera.main.WorldToScreenPoint(this.transform.position);
-                var pointerDirection = ballViewPortPos - mouseviewportPos;
-                pointerDirection.z = 0;
-
-                // //draw aim
-                // aimLine.transform.position = ballScreenPos;
-                // var positions = new Vector3[]{ballScreenPos,Input.mousePosition};
-                // aimLine.SetPositions(positions);
-
-                //force direction 
+            if(Input.GetMouseButtonDown(0))
+            {
+                aimLine.gameObject.SetActive(true);
+                aimWorld.gameObject.SetActive(true);
+                plane = new Plane(Vector3.up, this.transform.position);
+            }
+            else if(Input.GetMouseButton(0))
+            {
+                // force direction
                 ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                plane.Raycast(ray,out var distance);
+                plane.Raycast(ray, out var distance);
                 forceDirection = (this.transform.position - ray.GetPoint(distance));
                 forceDirection.Normalize();
 
-                // force factor                
-                forceFactor = pointerDirection.magnitude * 2;     
+                // force factor
+                var mouseViewportPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                var ballViewportPos = Camera.main.WorldToViewportPoint(this.transform.position);
+                var pointerDirection = ballViewportPos - mouseViewportPos;
+                pointerDirection.z = 0;
+                pointerDirection.z *= Camera.main.aspect;
+                pointerDirection.z = Mathf.Clamp(pointerDirection.z, -0.5f, 0.5f);
+                forceFactor = pointerDirection.magnitude * 2;
 
-                //aim visual
+                // aim visuals
                 aimWorld.transform.position = this.transform.position;
                 aimWorld.forward = forceDirection;
-                aimWorld.localScale = new Vector3(1,1,0.5f + forceFactor);
+                aimWorld.localScale = new Vector3(1, 1, 0.5f + forceFactor);
 
-          }
-          else if (Input.GetMouseButtonUp(0))
-          {
+                var ballScreenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+                var mouseScreenPos = Input.mousePosition;
+                ballScreenPos.z = 1f;
+                mouseScreenPos.z = 1f;
+
+                var positions = new Vector3[]{
+                    Camera.main.ScreenToWorldPoint(ballScreenPos),
+                    Camera.main.ScreenToWorldPoint(mouseScreenPos)};
+                aimLine.SetPositions(positions);
+                aimLine.endColor = Color.Lerp(Color.blue, Color.red, forceFactor);
+            }
+            else if(Input.GetMouseButtonUp(0))
+            {
                 shoot = true;
                 shootingMode = false;
-                // aimLine.gameObject.SetActive(false);
+                aimLine.gameObject.SetActive(false);
                 aimWorld.gameObject.SetActive(false);
-
-          }
+            }
         }
-     }
+    }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         if(shoot)
         {
             shoot = false;
-            rb.AddForce(forceDirection * force * forceFactor, ForceMode.Impulse);
+            AddForce(forceDirection * force * forceFactor, ForceMode.Impulse);
+            shootCount += 1;
+            onBallShooted.Invoke(shootCount);
         }
 
-        if(rb.velocity.sqrMagnitude < 0.01f && rb.velocity.sqrMagnitude > 0 )
+        if(rb.velocity.sqrMagnitude < 0.01f && rb.velocity.sqrMagnitude != 0)
         {
             rb.velocity = Vector3.zero;
+            rb.useGravity = false;
         }
     }
 
     public bool IsMove()
     {
         return rb.velocity != Vector3.zero;
+    }
+
+    public void AddForce(Vector3 force, ForceMode forceMode = ForceMode.Impulse)
+    {
+        rb.useGravity = true;
+        rb.AddForce(force, forceMode);
     }
 
     void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
